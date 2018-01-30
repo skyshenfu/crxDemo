760aa42ddf211c4ca5cb2beb4188192aa6ef69c1
painworkpc <zhangty@all-help.com>
2018-01-30 16:48:07 +0800

    新建立了个简单插件

---
+++ js/content.js
@@ -0,0 +1,11 @@
+setTimeout(
+   dealWithDom		
+,1000);
+
+function dealWithDom(){
+	$(document).ready(function(){
+	  $('span.btn___1_FJ-:contains("导入")').css({"background-color":"green","color":"white"}).click()
+})
+	
+	 //location.reload()
+}
